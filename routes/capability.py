b2e8a411d6fbbd9699ccdbb556d36d8f7afef55d
Isaac Meck <icedragonzclawz@gmail.com>
2020-09-09 14:31:39 -0400

    Updated the capabilities endpoint

--- routes/capability.py
+++ routes/capability.py
@@ -1,5 +1,5 @@
 from models import Capability
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from util import Util
 
 capability_endpoint = Blueprint('capability_endpoint',
@@ -9,3 +9,30 @@ capability_endpoint = Blueprint('capability_endpoint',
 @capability_endpoint.route('/capabilities', methods=['GET'])
 def get_all_capabilities():
     return Util.basic_get_request(Capability)
+
+@capability_endpoint.route('/capabilities', methods=['POST'])
+def create_resource():
+    body = Util.get_body(request)
+
+    number = body.get('number')
+    name = body.get('name')
+    size = body.get('size')
+    status = body.get('status')
+
+    try:
+        new_capability = Capability(number, name, size,
+                                  status)
+        new_capability.insert()
+        return jsonify({
+            'success': True,
+            'created': new_capability.id,
+        })
+    except Exception as e:
+        print(e)
+        abort(422)
+
+@capability_endpoint.route('/capabilities/<capability_id>', 
+                         methods=['DELETE'])
+def delete_project(capability_id):
+    return Util.basic_delete_request(Capability, 
+                                        capability_id)
